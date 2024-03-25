6d322d3201e8599bb290a1e3408e181344692b2d
SvetlanaRiasnova <R_S_U@outlook.com>
2024-03-25 21:30:49 +1000

    Переоформлен вывод результата

--- script1.js
+++ script1.js
@@ -5,14 +5,14 @@
 
 let tepmC = 35;
 let tempF = (9 / 5) * tepmC + 32;
-console.log(tepmC);
-console.log(tempF);
+console.log(`Температура в Цельсиях : ${tepmC}`);
+console.log(`Температура в Фаренгейтах : ${tempF}`);
 
 /*Задание 2
 Необходимо создать переменную name, записать в эту переменную свое имя. Необходимо также создать переменную admin и присвоить этой переменной значение из переменной name. Вывести значение переменной admin в консоль.*/
 
-let name = "Sveta";
+let name = "Svetlana";
 let admin;
 admin = name;
-console.log(admin);
+console.log(`Администратор ${admin}`);
 
